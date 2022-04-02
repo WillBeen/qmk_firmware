3e4dd1aedb8bb7a38d4acd5ddd716587b68fe4ce
DELGADO David <david.delgado.externe@foncia.com>
2022-04-03 00:35:05 +0200

    retro tapping

--- keyboards/crkbd/keymaps/willbeen/config.h
+++ keyboards/crkbd/keymaps/willbeen/config.h
@@ -32,7 +32,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 //#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 100
+#define TAPPING_TERM 50
+#define RETRO_TAPPING
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
