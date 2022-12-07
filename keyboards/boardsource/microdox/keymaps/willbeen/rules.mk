1cf49dd57ab3bf81b31c1614f71ff2612009f5e0
David Delgado <david.delgdao@gekko.fr>
2022-12-07 09:30:40 +0100

    stable with std/mod layers working

---
+++ keyboards/boardsource/microdox/keymaps/willbeen/rules.mk
@@ -0,0 +1,12 @@
+RGBLIGHT_ENABLE = no       # Disable keyboard RGB underglow
+MOUSEKEY_ENABLE = yes  # Enable mouse keys
+LTO_ENABLE      = yes  # Reduce firmware size
+
+COMMAND_ENABLE = no
+# MOUSEKEY_ENABLE = no
+EXTRAKEY_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+AVR_USE_MINIMAL_PRINTF = yes
+MUSIC_ENABLE = no
